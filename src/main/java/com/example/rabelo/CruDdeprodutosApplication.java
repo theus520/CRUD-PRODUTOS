4a7fd4db1c8642eff1ce2589857760302788aa14
Matheus rabelo <matheusrabelo520@gmail.com>
2024-01-12 18:48:42 -0300

    Criando o projeto

---
+++ src/main/java/com/example/rabelo/CruDdeprodutosApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDdeprodutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruDdeprodutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rabelo/CruDdeprodutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruDdeprodutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
