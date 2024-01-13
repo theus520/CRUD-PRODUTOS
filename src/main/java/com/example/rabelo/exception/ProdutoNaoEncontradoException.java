b0f6ebecd0f483723544d911d05bb8ada50d157c
Matheus rabelo <matheusrabelo520@gmail.com>
2024-01-12 22:28:15 -0300

    Adicionado a conexão com o banco H2

--- src/main/java/com/example/rabelo/exception/ProdutoNaoEncontradoException.java
+++ src/main/java/com/example/rabelo/exception/ProdutoNaoEncontradoException.java
@@ -2,7 +2,6 @@ package com.example.rabelo.exception;
 
 public class ProdutoNaoEncontradoException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
-
 	public ProdutoNaoEncontradoException(String mensagem) {
 		super(mensagem);
 	}
