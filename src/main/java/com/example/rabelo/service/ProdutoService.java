e7877a70933012aa82a4ed91b0f0f681902d5364
Matheus rabelo <matheusrabelo520@gmail.com>
2024-01-12 21:44:17 -0300

    Criando a camada de serviço

--- src/main/java/com/example/rabelo/service/ProdutoService.java
+++ src/main/java/com/example/rabelo/service/ProdutoService.java
@@ -1,5 +1,25 @@
 package com.example.rabelo.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.rabelo.entidade.Produto;
+import com.example.rabelo.repository.ProdutoRepository;
+
 public class ProdutoService {
 
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	public List<Produto> getAllProdutos(){
+		return produtoRepository.findAll();
+	}
+	
+	public Produto getProdutoById(Long id) {
+		return produtoRepository.findById(id).orElse(null);
+	}
+	public Produto saveProduto(Produto produto) {
+		return produtoRepository.save(produto);
+	}
 }
